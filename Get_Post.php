ba8ec0412011bbca3a350b51a357a479167f836d
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-30 17:59:03 +0530

    Display user input values in Get_Post.php

--- Get_Post.php
+++ Get_Post.php
@@ -8,7 +8,11 @@
 <body>
     <?php
     
-       
+       echo $_POST['name'];
+       echo "<br>";
+       echo $_POST['email'];
+       echo "<br>";
+       echo $_POST['password'];
     
     
     ?>
