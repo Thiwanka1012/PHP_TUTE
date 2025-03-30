a5a59ee24f29c114666765231e5c2304ae6e5ab5
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2025-03-30 17:11:17 +0530

    Add Get_Post.php with basic HTML structure and a form for user input

---
+++ Get_Post.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+       
+    
+    
+    ?>
+
+<form method="GET">
+
+    <input type="hidden" name="name" value="Thiwanka Umesh">
+    <input type="text" name= "User Input"> 
+
+</form>
+
+</body>
+</html>
